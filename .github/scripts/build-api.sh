ae62c6899b39a4661a603f07548899f8a318b3f1
Dieter Luypaert <dieterluypaert@gmail.com>
2021-01-07 13:17:27 +0100

    🚀 setup deployment pipeline

--- .github/scripts/build-api.sh
+++ .github/scripts/build-api.sh
@@ -3,6 +3,5 @@
 yarn workspace api run generate:prisma
 cp -r apps/api/node_modules/.prisma ./node_modules
 
-# Build and deploy the API code
+# Build API code
 yarn nx run api:build --configuration=production
-yarn serverless deploy
