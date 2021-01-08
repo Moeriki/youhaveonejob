c5a656f359f45bd0e0fdd4cb278bdad8f07d1fc7
Dieter Luypaert <dieterluypaert@gmail.com>
2021-01-08 17:01:34 +0100

    🐛 render document correctly
    - adapt to styled components example of Next.js examples
    - use next/document/Html component
    
    References:
    - https://nextjs.org/docs/advanced-features/custom-document
    - https://github.com/vercel/next.js/tree/canary/examples/with-styled-components
    
    This fixes the "several instances of 'styled-components'" error.

--- apps/web-app/pages/_document.tsx
+++ apps/web-app/pages/_document.tsx
@@ -1,31 +1,44 @@
 import React, { ReactElement } from 'react';
-import Document, { Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 import { ServerStyleSheet } from 'styled-components';
 
 export default class CustomDocument extends Document<{
   styleTags: ReactElement[];
 }> {
-  static getInitialProps({ renderPage }) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
 
-    const page = renderPage((App) => (props) =>
-      sheet.collectStyles(<App {...props} />)
-    );
-
-    const styleTags = sheet.getStyleElement();
-
-    return { ...page, styleTags };
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
     return (
-      <html>
+      <Html>
         <Head>{this.props.styleTags}</Head>
         <body>
           <Main />
           <NextScript />
         </body>
-      </html>
+      </Html>
     );
   }
 }
